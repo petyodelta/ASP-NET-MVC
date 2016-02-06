1580b29ea36e9fcbf182e4e7ed30578c56c6ca71
petyodelta <petyodelta@gmail.com>
2016-02-06 16:03:57 +0200

    Moved DbContext to OrdersSystem.Data; Moved User to OrdersSystem.Models; Installed Entity Framework in whole solution; Installed Identity to Data and Models;

---
+++ Orders/OrdersSystem/Data/OrdersSystem.Data/OrdersDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OrdersSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersSystem.Data
+{
+    public class OrdersDbContext : IdentityDbContext<User>
+    {
+        public OrdersDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static OrdersDbContext Create()
+        {
+            return new OrdersDbContext();
+        }
+    }
+}
--- Orders/OrdersSystem/Web/OrdersSystem.Web/Controllers/AccountController.cs
+++ Orders/OrdersSystem/Web/OrdersSystem.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using OrdersSystem.Web.Models;
+using OrdersSystem.Models;
 
 namespace OrdersSystem.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace OrdersSystem.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace OrdersSystem.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
