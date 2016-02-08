074f729540c50ff647f8d995e384b780360fa462
petyodelta <petyodelta@gmail.com>
2016-02-08 14:28:25 +0200

    Edit Supplier model; Added Ninject to Web project;

--- Orders/OrdersSystem/Models/OrdersSystem.Models/Supplier.cs
+++ Orders/OrdersSystem/Models/OrdersSystem.Models/Supplier.cs
@@ -1,14 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace OrdersSystem.Models
+﻿namespace OrdersSystem.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class Supplier
     {
+        private ICollection<OutOrder> outOrders;
+
+        public Supplier()
+        {
+            this.outOrders = new HashSet<OutOrder>();
+        }
         public int Id { get; set; }
 
         [Required]
         [MinLength(3)]
         [MaxLength(150)]
         public string Name { get; set; }
+
+        public virtual ICollection<OutOrder> OutOrders
+        {
+            get { return this.outOrders; }
+            set { this.outOrders = value; }
+        }
+
     }
 }
