7112168405086bce15dfa339405154c609d6c024
petyodelta <petyodelta@gmail.com>
2016-02-11 00:20:12 +0200

    Installed Glimpse; updated some packages and removed respond, modernizr;

--- Orders/OrdersSystem/Models/OrdersSystem.Models/User.cs
+++ Orders/OrdersSystem/Models/OrdersSystem.Models/User.cs
@@ -1,10 +1,6 @@
 ﻿namespace OrdersSystem.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Security.Claims;
-    using System.Text;
     using System.Threading.Tasks;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
