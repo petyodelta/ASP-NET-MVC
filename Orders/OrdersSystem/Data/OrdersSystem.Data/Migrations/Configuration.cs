b5efefd425ea0b1f5935d4d44a36f374d35c315a
petyodelta <petyodelta@gmail.com>
2016-02-12 00:48:44 +0200

    SeedCategories, SeedCustomers, SeedDevices, SeedRoles and SeedUsers;

--- Orders/OrdersSystem/Data/OrdersSystem.Data/Migrations/Configuration.cs
+++ Orders/OrdersSystem/Data/OrdersSystem.Data/Migrations/Configuration.cs
@@ -1,7 +1,10 @@
 namespace OrdersSystem.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System.Data.Entity.Migrations;
-
+    using System.Linq;
     public sealed class Configuration : DbMigrationsConfiguration<OrdersDbContext>
     {
         public Configuration()
@@ -12,18 +15,137 @@ namespace OrdersSystem.Data.Migrations
 
         protected override void Seed(OrdersDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            this.SeedRoles(context);
+            this.SeedUsers(context);
+            this.SeedCategories(context);
+            this.SeedDevices(context);
+            this.SeedCustomers(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void SeedRoles(OrdersDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                var adminRole = new IdentityRole { Name = "Admin" };
+                roleManager.Create(adminRole);
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedUsers(OrdersDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userManager = new UserManager<User>(new UserStore<User>(context));
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+                var admin = new User()
+                {
+                    Email = "admin@admin.com",
+                    UserName = "admin@admin.com"
+                };
+
+                userManager.Create(admin, "123456");
+                userManager.AddToRole(admin.Id, "Admin");
+
+                var worker = new User()
+                {
+                    Email = "pesho@pesho.com",
+                    UserName = "pesho@pesho.com"
+                };
+
+                var workerRole = roleManager.Create(new IdentityRole("Worker"));
+                userManager.Create(worker, "123456");
+                userManager.AddToRole(worker.Id, "Worker");
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCategories(OrdersDbContext context)
+        {
+            string[] cats = new string[] { "Indicators", "Controllers", "Transmitters", "Gas Alarms" };
+            if (!context.Categories.Any())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var category = new Category()
+                    {
+                        Name = cats[i]
+                    };
+
+                    context.Categories.Add(category);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedDevices(OrdersDbContext context)
+        {
+            string[] indicators = new string[] { "4002", "4003", "4004", "4007" };
+            string[] controllers = new string[] { "4006", "2002", "6002", "3002" };
+            string[] transmitters = new string[] { "DI-DPT", "SP-410L", "SP-520L", "RHT-RS" };
+            string[] gasAlarms = new string[] { "DG-910", "DG-510", "DG-2000", "OXY meter" };
+            if (!context.Devices.Any())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var indicator = new Device()
+                    {
+                        Name = indicators[i],
+                        CategoryId = 1
+                    };
+                    context.Devices.Add(indicator);
+
+                    var controller = new Device()
+                    {
+                        Name = controllers[i],
+                        CategoryId = 2
+                    };
+                    context.Devices.Add(controller);
+
+                    var transmitter = new Device()
+                    {
+                        Name = transmitters[i],
+                        CategoryId = 3
+                    };
+                    context.Devices.Add(transmitter);
+
+                    var gas = new Device()
+                    {
+                        Name = gasAlarms[i],
+                        CategoryId = 4
+                    };
+                    context.Devices.Add(gas);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCustomers(OrdersDbContext context)
+        {
+            if (!context.Customers.Any())
+            {
+                string[] customers = new string[] { "Delektra", "Enemona", "Tasi", "Sofiiska voda" };
+                for (int i = 0; i < 4; i++)
+                {
+                    var customer = new Customer()
+                    {
+                        Name = customers[i],
+                        Address = "Address " + i
+                    };
+
+                    context.Customers.Add(customer);
+                }
+
+                context.SaveChanges();
+            }
         }
     }
 }
