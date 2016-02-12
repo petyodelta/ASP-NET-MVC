d0b806ad288608d732ad8359191f3f38d3e61716
petyodelta <petyodelta@gmail.com>
2016-02-12 11:10:49 +0200

    Implemented cache on home page; Added partial view for cached incoming orders;

--- Orders/OrdersSystem/Web/OrdersSystem.Web/Controllers/HomeController.cs
+++ Orders/OrdersSystem/Web/OrdersSystem.Web/Controllers/HomeController.cs
@@ -1,12 +1,33 @@
-﻿using System.Web.Mvc;
-
-namespace OrdersSystem.Web.Controllers
+﻿namespace OrdersSystem.Web.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+    using OrdersSystem.Services.Contracts;
+    using OrdersSystem.Web.Infrastructure.Mapping;
+    using Ninject;
+    using ViewModels.InOrders;
     public class HomeController : Controller
     {
+        [Inject]
+        public IInOrdersServices InOrdersServices { get; set; }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [OutputCache(Duration = 1 * 60)]
+        [ChildActionOnly]
+        public ActionResult CacheHome()
+        {
+            var inOrders = this.InOrdersServices
+                .GetAll()
+                .OrderByDescending(x => x.EndDate)
+                .Take(6)
+                .To<InOrderViewModel>()
+                .ToList();
+
+            return this.PartialView("_CacheHomePartial", inOrders);
         }       
     }
 }
