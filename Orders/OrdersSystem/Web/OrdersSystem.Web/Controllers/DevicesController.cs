ece9ef19ff8c263c795ba2a99aa0b361f682fdc9
petyodelta <petyodelta@gmail.com>
2016-02-15 14:56:42 +0200

    Devices now lists with cache and partial

---
+++ Orders/OrdersSystem/Web/OrdersSystem.Web/Controllers/DevicesController.cs
@@ -0,0 +1,31 @@
+﻿namespace OrdersSystem.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using OrdersSystem.Services.Contracts;
+    using OrdersSystem.Web.Infrastructure.Mapping;
+    using Ninject;
+    using ViewModels.Devices;
+
+    public class DevicesController : BaseController
+    {
+        [Inject]
+        public IDevicesServices DevicesServices { get; set; }
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 1 * 60)]
+        [ChildActionOnly]
+        public ActionResult CacheDevices()
+        {
+            var devices = this.DevicesServices
+                .GetAll()
+                .To<DeviceViewModel>();
+
+            return this.PartialView("_AllDevicesPartial", devices);
+        }
+    }
+}
